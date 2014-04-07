20f739ac2249c969d93fe2447598ce075f2e71ec
Steffen Larsen <zteffenz@gmail.com>
2014-04-07 22:28:13 +0200

    Second

--- NumRec.hs
+++ NumRec.hs
@@ -1,15 +1,23 @@
 import System.Directory
+import Data.Char
+import Data.List
 
+learnFromFile :: String -> IO ()
+learnFromFile file = return 3
+	where
+		isBitmap path =
+			doesFileExist path >>=
+			\exist -> exist && ".bmp" `isSuffixOf` path
+	
 learnAllFromDirectory :: String -> IO ()
 learnAllFromDirectory dir = 
 	getDirectoryContents dir >>=
-	
+	\contents -> 
 	where
-		
-	
+		fullPath file = dir ++ "\\" ++ file
 
 learn :: IO()
 learn = 
-	putStrLine "Enter the destination of the directory with the files. (Make sure that the filenames starts with the number that it represent.)" >>
+	putStrLn "Enter the destination of the directory with the files. (Make sure that the filenames starts with the number that it represent.)" >>
 	getLine >>=
-	learnAllFromDirectory
+	\dir -> learnAllFromDirectory dir
